1df10feeb5d8803db3ab032bcaa7e52add4851da
ye231101 <ye231101@gmail.com>
2025-10-22 14:40:19 -0400

    change the file name

--- components/category-attributes-card.tsx
+++ components/category-attributes-card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { CategorySelectionForm } from './CategorySelectionForm';
-import { DynamicAttributesForm } from './DynamicAttributesForm';
+import { CategorySelectionForm } from './category-selection-form';
+import { DynamicAttributesForm } from './dynamic-attributes-form';
 
 interface Attribute {
   id: string;
