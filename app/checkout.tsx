8f15278a99a12cae92711f7661956568ff6147a7
ye231101 <ye231101@gmail.com>
2025-10-28 19:42:56 -0400

    fix the postal code required

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -383,6 +383,7 @@ export default function CheckoutScreen() {
                     onChangeText={(text) => updateShippingAddress('postcode', text)}
                     placeholder="Postal Code"
                     autoCapitalize="none"
+                    required
                   />
                 </View>
               </View>
